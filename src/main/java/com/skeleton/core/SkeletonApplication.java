37a10157b703585c3d6c6c6fcc4a6f66736c0255
xinyuwuhen1988 <837725417@qq.com>
2018-07-27 15:51:33 +0800

    创建框架项目

---
+++ src/main/java/com/skeleton/core/SkeletonApplication.java
@@ -0,0 +1,12 @@
+package com.skeleton.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkeletonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkeletonApplication.class, args);
+	}
+}
